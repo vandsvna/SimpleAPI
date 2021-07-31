4201df9acd9a23c0b6162eb91b4dd75016fbdd57
vandsvna0170 <vandsvna@gmail.com>
2021-07-31 19:05:15 +0530

    Phase 1

---
+++ Test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        // Arrange
+        WeatherForecastController oWFC = new WeatherForecastController();
+        [Fact]
+        public void Test1()
+        {
+            // Act
+            var oResult = oWFC.GetOwnerName(1);
+
+            // Assert
+            Assert.Equal("Vandana Sharma", oResult);
+        }
+    }
+}
